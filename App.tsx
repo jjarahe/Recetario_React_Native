b7f0a06f6ce17d1ac03e4d5aaa170b4a0e30f1a6
Juan Jara <jjarahe@gmail.com>
2022-05-08 12:41:54 -0600

    Sign In Sign Out screens

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import 'react-native-gesture-handler';
 import React from 'react'
+import { ThemeProvider } from 'react-native-elements';
 import './config/firebase'
 import RootNavigation from './navigation';
 
 
 export default function App() {
   return (
-      <RootNavigation />
+      <ThemeProvider>
+           <RootNavigation />
+      </ThemeProvider>
+     
     /*<View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
--- screens/SignUpScreen.tsx
+++ screens/SignUpScreen.tsx
@@ -1,12 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input, Button } from 'react-native-elements';
+import { StackScreenProps } from '@react-navigation/stack';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
-export default function SignUpScreen() {
-  return (
+
+
+const SignUpScreen: React.FC<StackScreenProps<any>> = ({ navigation }) => {
+    const [value, setValue] = React.useState({
+        email: '',
+        password: '',
+        error: ''    
+    });
+    
+    async function signUp() {
+        if (value.email === '' || value.password === '') {
+            setValue({
+                ...value,
+                error: 'Email and password are mandatory.'
+            })
+            return;
+        }
+        
+        try {
+            const auth = getAuth();
+            await createUserWithEmailAndPassword(auth, value.email, value.password);
+            navigation.navigate('Sign In');
+        } catch (error) {
+            setValue({
+                ...value,
+                error: error.message,
+            })
+        } 
+    }
+    
+    return (
     <View style={styles.container}>
       <Text>SignUpScreen screen!</Text>
-      <StatusBar style="auto" />
+      
+      {!!value.error && <View style={styles.error}><Text>{ value.error }</Text></View>}
+      
+      <View style={styles.controls}>
+          <Input
+            placeholder='Email'
+            containerStyle={styles.control}
+            value={value.email}
+            onChangeText={(Text) => setValue({...value, email: Text})}
+            leftIcon={<Icon
+                        name='envelope'
+                        size={16}
+                        />}
+          />
+          
+          <Input
+          placeholder='Password'
+          containerStyle={styles.control}
+          value={value.password}
+          onChangeText={(text) => setValue({ ...value, password: text })}
+          secureTextEntry={true}
+          leftIcon={<Icon
+            name='key'
+            size={16}
+          />}
+        />
+
+        <Button title="Sign up" buttonStyle={styles.control} onPress={signUp} />
+          
+      </View> 
     </View>
   );
 }
@@ -14,8 +75,27 @@ export default function SignUpScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  controls: {
+      flex: 1,
+  },
+  
+  control: {
+    marginTop: 10,
+    width: 300,
+  },
+  
+  error: {
+      marginTop: 10,
+      padding: 10,
+      color: '#fff',
+      backgroundColor: '#D54826FF',
+  }
 });
+
+export default SignUpScreen;
